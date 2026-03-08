c7543205d28cd46a88fd5250c27c6bfea3079582
びゅな <153439719+tuitta-mindayo@users.noreply.github.com>
2026-03-09 02:42:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ const form = document.getElementById("CustomForm");
 const text = document.getElementById("txt");
 
 container.addEventListener("click", (event) => {
+  const target = event.target;
+  
   if (target.tagName !== "BUTTON") return;
 
   const id = target.id;
-  const num = id.replace("ButtonConst", ""); 
 
   if (id.startsWith("ButtonConst")) {
+    const num = id.replace("ButtonConst", ""); 
     text.textContent = num;
   } 
-  // 2. 残り1つの特殊なボタンの判定
   else if (id === "ButtonCustom" && form.value != "")
   {
     text.textContent = form.value;
